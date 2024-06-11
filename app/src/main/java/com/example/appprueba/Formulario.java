a781462afcc5fc522c611eda509a7200c0710ae0
Omar Meza <omeza3.14@hotmail.com>
2024-06-10 21:54:52 -0500

    septimo commit

--- app/src/main/java/com/example/appprueba/Formulario.java
+++ app/src/main/java/com/example/appprueba/Formulario.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +23,9 @@ public class Formulario extends AppCompatActivity {
 
     TextView psswd;
 
+    RadioButton rb1,rb2;
+    CheckBox cb1,cb2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +39,11 @@ public class Formulario extends AppCompatActivity {
 
         user = findViewById(R.id.txtUser);
         psswd = findViewById(R.id.txtPassw);
+        rb1 = findViewById(R.id.rdb1);
+        rb2 = findViewById(R.id.rdb2);
+        cb1 = findViewById(R.id.cbx1);
+        cb2 = findViewById(R.id.cbx2);
+
         spinner = (Spinner) findViewById(R.id.spCursos);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
@@ -54,6 +64,37 @@ public class Formulario extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+
+    public void Registrar(View view){
+
+        Intent i = new Intent(this, MainActivity.class);
+        String usu = user.getText().toString();
+        String pwd = user.getText().toString();
+        String curso = spinner.getSelectedItem().toString();
+        String sexo = "";
+        String ciclo="";
+        if(rb1.isChecked())
+            sexo = "Masculino";
+        else
+            sexo = "Femenino";
+
+        if(cb1.isChecked())
+            ciclo = "CICLO I";
+        if (cb1.isChecked())
+            ciclo = "CICLO II";
+
+        Toast.makeText(this, "Usuario: "+usu+"\n"+
+                       "Contraseña:" + pwd +"\n"+
+                        "Curso:" + curso + "\n"+
+                        "Sexo:" + sexo + "\n"+
+                        "Ciclo:" + ciclo + "\n", Toast.LENGTH_SHORT).show();
+
+        i.putExtra("user", usu);
+        i.putExtra("psswd", pwd);
+        startActivity(i);
+        finish();
+    }
+
 
     public void Mostrar(View view){
         Intent i = new Intent(this, MainActivity.class);
